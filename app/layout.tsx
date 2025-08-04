3db353dcbc4f5131ce62d71dfdf7063205ad837d
anggi.anggoro26@gmail.com <anggi.anggoro26@gmail.com>
2025-08-04 23:03:52 +0700

    Adjust page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,6 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
-import { hasEnvVars } from "@/lib/utils";
-import { AuthButton } from "@/components/auth-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import Logo from "@/app/assets/m.png";
-import WhatsAppButton from "@/components/whatsapp";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
-import { HeaderLinks } from "@/components/header-links";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -26,13 +18,6 @@ const geistSans = Geist({
   display: "swap",
   subsets: ["latin"],
 });
-const authContent = !hasEnvVars ? (
-  <EnvVarWarning />
-) : (
-  <div>
-    <AuthButton />
-  </div>
-);
 
 export default function RootLayout({
   children,
@@ -41,15 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.className} antialiased`}>
-        <Header
-          logo={Logo}
-          authContent={authContent}
-          headerLinks={<HeaderLinks />}
-        />
-        <WhatsAppButton />
+      <body className={`${geistSans.className} antialiased`}>        
         {children}
-        <Footer />
+        
       </body>
     </html>
   );
--- app/module/layout.tsx
+++ app/module/layout.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { redirect, useRouter } from "next/navigation";
-import SidebarLeft from "@/components/navbar";
-import SidebarRight from "@/components/sidebar";
-import { createClient } from "@/lib/supabase/client"; // client-side
 import PopupText from "@/components/popup-text";
 
 export default function ModuleLayout({
@@ -13,32 +9,9 @@ export default function ModuleLayout({
   children: React.ReactNode;
 }) {
   const [showSidebar, setShowSidebar] = useState(false);
-  const [showPopUp, setShowPopUp] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
-  const router = useRouter();
+  const [showPopUp] = useState(false);
 
-  useEffect(() => {
-    const supabase = createClient();
 
-    supabase.auth.getUser().then(({ data, error }) => {
-      if (error || !data.user) {
-        return redirect("/auth/login");
-      } else {
-        supabase
-          .from("userdata")
-          .select("pretest_score")
-          .eq("email", data.user.email)
-          .single()
-          .then(({ data: userData, error: userError }) => {
-            if (userError || !userData.pretest_score) {
-              setIsAuthenticated(false);
-            } else {
-              setIsAuthenticated(true);
-            }
-          });
-      }
-    });
-  }, [router]);
 
   // Tombol close sidebar (custom event)
   useEffect(() => {
@@ -47,20 +20,6 @@ export default function ModuleLayout({
     return () =>
       window.removeEventListener("closeSidebar", closeSidebarHandler);
   }, []);
-
-  if (isAuthenticated === null) {
-    return <div className=" min-h-svh p-10">Loading...</div>;
-  } else if (!isAuthenticated) {
-    return (
-      <div className="min-h-[60vh]">
-        <PopupText
-          url="/pretest"
-          message="Kerjakan Pre-test terlebih dahulu untuk mengakses modul"
-          buttonText="Kerjakan Sekarang"
-        />
-      </div>
-    );
-  }
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -81,31 +40,10 @@ export default function ModuleLayout({
       </div>
 
       <div className="flex-1 flex flex-col md:flex-row relative">
-        <div
-          className={`
-            fixed md:static top-0 left-0 min-h-full z-40 bg-white pt-4 pl-4
-            transform transition-transform duration-300 ease-in-out
-            ${showSidebar ? "translate-x-0" : "-translate-x-full"}
-            md:translate-x-0 md:basis-1/5 shrink-0
-          `}
-        >
-          <SidebarLeft isShowPopUp={setShowPopUp} />
-        </div>
-
-        {showSidebar && (
-          <div
-            className="fixed inset-0 bg-black bg-opacity-30 z-30 md:hidden"
-            onClick={() => setShowSidebar(false)}
-          />
-        )}
 
         <main className="w-full md:basis-3/5 p-4 overflow-y-auto scroll-smooth">
           {children}
         </main>
-
-        <div className="max-md:hidden md:basis-1/5 p-4  border-l shrink-0">
-          <SidebarRight />
-        </div>
       </div>
     </div>
   );
